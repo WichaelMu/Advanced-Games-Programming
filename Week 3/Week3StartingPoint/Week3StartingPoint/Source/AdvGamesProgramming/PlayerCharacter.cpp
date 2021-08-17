206a649706dfd58eb50a684f97848f39b5ef5fcd
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2021-08-17 14:53:53 +1000

    Week 3 Submission

---
+++ Week 3/Week3StartingPoint/Week3StartingPoint/Source/AdvGamesProgramming/PlayerCharacter.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	//Set default member variable values
+	LookSensitivity = 1.0f;
+
+	SprintMultiplier = 1.5f;
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Camera = FindComponentByClass<UCameraComponent>();
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Strafe"), this, &APlayerCharacter::Strafe);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APlayerCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APlayerCharacter::Turn);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"),EInputEvent::IE_Pressed, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAction(TEXT("Sprint"), EInputEvent::IE_Pressed, this, &APlayerCharacter::SprintStart);
+	PlayerInputComponent->BindAction(TEXT("Sprint"), EInputEvent::IE_Released, this, & APlayerCharacter::SprintEnd);
+}
+
+void APlayerCharacter::MoveForward(float Value) 
+{
+	FRotator ForwardRotation = GetControlRotation();
+	ForwardRotation.Roll = 0.0f;
+	ForwardRotation.Pitch = 0.0f;
+	AddMovementInput(ForwardRotation.Vector(), Value);
+}
+
+void APlayerCharacter::Strafe(float Value) 
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void APlayerCharacter::LookUp(float Value) 
+{
+	FRotator LookUpRotation = FRotator::ZeroRotator;
+	LookUpRotation.Pitch = Value * LookSensitivity;
+	Camera->AddRelativeRotation(LookUpRotation);
+	Camera->RelativeRotation.Roll = 0.f;
+	Camera->RelativeRotation.Yaw = 0.f;
+}
+
+void APlayerCharacter::Turn(float Value) 
+{
+	AddControllerYawInput(Value * LookSensitivity);
+}
+
+void APlayerCharacter::SprintStart()
+{
+	GetCharacterMovement()->MaxWalkSpeed *= SprintMultiplier;
+}
+
+void APlayerCharacter::SprintEnd()
+{
+	GetCharacterMovement()->MaxWalkSpeed /= SprintMultiplier;
+}
+
